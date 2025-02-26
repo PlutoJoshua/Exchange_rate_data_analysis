56515fe373d3f90cc4dd7d0d55a788f4ffee6649
PlutoJoshua <thdude37@gmail.com>
2025-02-26 14:16:19 +0900

    add plot_weekly_subplots

--- ex_analysis.py
+++ ex_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from data import load_data, sort_data
 from visual import plot_time_series_px
+import plotly.express as px
 import streamlit as st
 
 df = load_data()
@@ -10,11 +11,20 @@ usd = sort_data(df, 'USD')
 jpy = sort_data(df, 'JPY')
 # print(jpy.describe())
 
+# 환율 변화 차이(전 값과 비교, 절대값)
+usd.loc[:, 'diff'] = usd['basePrice'].diff().abs()
+jpy.loc[:, 'diff'] = jpy['basePrice'].diff().abs()
+
 st.header("Ex analysis Dashboard")
+# 통화 선택 드롭다운 추가
+currency = st.sidebar.selectbox("Select Currency", ['USD', 'JPY'])
 
 tab1, tab2 = st.tabs(['basic', 'diff'])
 
 with tab1:
-    st.plotly_chart(plot_time_series_px(usd, 'USD'))
-    st.plotly_chart(plot_time_series_px(jpy, 'JPY'))
-
+    if currency == 'USD':
+        st.plotly_chart(plot_time_series_px(usd, 'USD'))
+        st.plotly_chart(px.line(usd, x='createdAt', y='diff', title='USD'))
+    elif currency == 'JPY':
+        st.plotly_chart(plot_time_series_px(jpy, 'JPY'))
+        st.plotly_chart(px.line(jpy, x='createdAt', y='diff', title='JPY'))
--- visual.py
+++ visual.py
@@ -1,4 +1,6 @@
 import plotly.express as px
+import plotly.subplots as sp
+import plotly.graph_objects as go
 
 # function
 def plot_time_series_px(df, currency_code):
@@ -6,3 +8,24 @@ def plot_time_series_px(df, currency_code):
     fig.update_layout(xaxis_title='Date', yaxis_title='Base Price')
     return fig
 
+def plot_weekly_subplots(df):
+    df['week'] = df['createdAt'].dt.to_period('W').astype(str)
+    weeks = df['week'].unique()
+    num_weeks = len(weeks)
+
+    num_cols = 1
+    num_rows = (num_weeks + num_cols - 1) // num_cols
+    fig = sp.make_subplots(rows=num_rows, cols=num_cols, subplot_titles=weeks)
+
+    for i, week in enumerate(weeks):
+        week_data = df[df['week'] == week]
+        fig.add_trace(
+            go.Scatter(x=week_data['createdAt'], y=week_data['diff'], mode='lines', name=week),
+            row=(i // num_cols) + 1,
+            col=(i % num_cols) + 1
+        )
+
+    fig.update_layout(title_text='weekly', height=600 * num_rows, showlegend=False)
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='diff')
+    return fig
