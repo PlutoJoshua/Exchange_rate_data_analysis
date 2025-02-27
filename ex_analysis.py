40f3a41be7101b94ccabe43da2386e77a11d230a
PlutoJoshua <thdude37@gmail.com>
2025-02-27 15:34:27 +0900

    주석 추가2

--- ex_analysis.py
+++ ex_analysis.py
@@ -15,17 +15,25 @@ jpy = sort_data(df, 'JPY')
 usd.loc[:, 'diff'] = usd['basePrice'].diff().abs()
 jpy.loc[:, 'diff'] = jpy['basePrice'].diff().abs()
 
+# streamlit setting
 st.header("Ex analysis Dashboard")
-# 통화 선택 드롭다운 추가
+
+# 통화 선택 드롭다운
 currency = st.sidebar.selectbox("Select Currency", ['USD', 'JPY'])
+
+# 시간 선택 드롭다운
 hour = st.sidebar.number_input("input number", min_value=1, value=3)
+
+# 탭 구분
 tab1, tab2 = st.tabs(['basic', 'diff'])
 
+# 탭 1 (기본)
 with tab1:
     if currency == 'USD':
         st.plotly_chart(plot_time_series_px(usd, 'USD'))
         st.markdown("---")
         st.plotly_chart(px.box(usd, y='basePrice', title='Box Plot of basePrice'))
+        st.markdown("---")
         st.plotly_chart(px.line(usd, x='createdAt', y='diff', title='USD'))
         st.markdown("---")        
         st.plotly_chart(plot_weekly_subplots(usd))
@@ -34,6 +42,7 @@ with tab1:
         st.plotly_chart(plot_time_series_px(jpy, 'JPY'))
         st.markdown("---")
         st.plotly_chart(px.box(jpy, y='basePrice', title='Box Plot of basePrice'))
+        st.markdown("---")
         st.plotly_chart(px.line(jpy, x='createdAt', y='diff', title='JPY'))
         st.markdown("---")
         st.plotly_chart(plot_weekly_subplots(jpy))
@@ -41,35 +50,47 @@ with tab1:
 with tab2:
     if currency == 'USD':
         st.plotly_chart(time_slot(usd))
-
+        st.markdown("---")
         # 원하는 시간대로 데이터 필터링
         filter_df = calculate_price_difference(usd, hour)
         st.plotly_chart(time_slot_price_diff(filter_df))
+        st.markdown("---")
         st.plotly_chart(plot_price_difference(filter_df))
+        st.markdown("---")
         df_resampled = usd.resample("1h", on='createdAt')['diff'].mean().dropna()
         st.plotly_chart(px.line(df_resampled, x=df_resampled.index, y=df_resampled.values))
+        st.markdown("---")
+        # 차이가 없는 비율 계산
         count_zero_diff = (filter_df['price_diff'] == 0.0).sum()  # price_diff가 0.0인 갯수
         total_count = len(filter_df)  # 전체 갯수
         st.markdown(f'Price difference == 0.0: {count_zero_diff} out of {total_count} / {(count_zero_diff / total_count).round(2) * 100} %')
+        st.markdown("---")
         st.plotly_chart(box_plot(filter_df))
         sort = filter_df.sort_values(by='price_diff', ascending=False)
         st.markdown("---")
         st.plotly_chart(plot_price_distribution_histogram(filter_df))
+        st.markdown("---")
         st.dataframe(sort)
     elif currency == 'JPY':
         st.plotly_chart(time_slot(jpy))
-        
+        st.markdown("---")
         # 원하는 시간대로 데이터 필터링
         filter_df = calculate_price_difference(jpy, hour)
         st.plotly_chart(time_slot_price_diff(filter_df))
+        st.markdown("---")
         st.plotly_chart(plot_price_difference(filter_df))
+        st.markdown("---")
         df_resampled = usd.resample("1h", on='createdAt')['diff'].mean().dropna()
         st.plotly_chart(px.line(df_resampled, x=df_resampled.index, y=df_resampled.values))
+        st.markdown("---")
+        # 차이가 없는 비율 계산
         count_zero_diff = (filter_df['price_diff'] == 0.0).sum()  # price_diff가 0.0인 갯수
         total_count = len(filter_df)  # 전체 갯수
-        st.markdown(f'Price difference == 0.0: {count_zero_diff} out of {total_count} / {(count_zero_diff / total_count).round(2) * 100} %')
+        st.markdown(f'**Price difference** == 0.0: {count_zero_diff} out of {total_count} / {(count_zero_diff / total_count).round(2) * 100} %')
+        st.markdown("---")
         st.plotly_chart(box_plot(filter_df))
         sort = filter_df.sort_values(by='price_diff', ascending=False)
         st.markdown("---")
         st.plotly_chart(plot_price_distribution_histogram(filter_df))
+        st.markdown("---")
         st.dataframe(sort)
